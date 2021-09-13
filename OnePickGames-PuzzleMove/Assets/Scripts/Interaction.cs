66310d6a4dcca6cac11b3847136e81174cce7b29
KimSoCheon <ulidkdlrhd12@naver.com>
2021-09-13 09:56:23 +0900

    Merge (main/PuzzleMove)

---
+++ OnePickGames-PuzzleMove/Assets/Scripts/Interaction.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Interaction : MonoBehaviour
+{
+    public Vector3 playerPos;
+    bool playerCollider = false;
+    Puzzle puzzle;
+    public GameObject puzzleGame;
+
+    void Start()
+    {
+        puzzle = GameObject.FindGameObjectWithTag("Puzzle").GetComponent<Puzzle>();
+        puzzleGame = GameObject.FindGameObjectWithTag("Puzzle");
+    }
+
+
+    void Update()
+    {
+        if (playerCollider)
+        {
+            puzzle.Puzzles();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerPos = other.gameObject.transform.position;
+            playerCollider = true;
+        }
+        else
+        {
+            playerCollider = false;
+        }
+    }
+}
