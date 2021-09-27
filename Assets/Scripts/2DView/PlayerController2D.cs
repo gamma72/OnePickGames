cc7ccf85a8465034cb162056abd031ebd110b325
KimSoCheon <ulidkdlrhd12@naver.com>
2021-09-27 13:11:31 +0900

    Map2

--- Assets/Scripts/2DView/PlayerController2D.cs
+++ Assets/Scripts/2DView/PlayerController2D.cs
@@ -10,6 +10,14 @@ public class PlayerController2D : MonoBehaviour
     [SerializeField]
     float cameraRange;
 
+    //스크립트 추가
+    Puzzle puzzle;
+    bool playerPosChange = false;
+    bool jumps = false;
+    float curTime = 0;
+    public float coolTime;
+    Transform playerPos;
+
     public bool lookDown;
     Vector3 cameraTarget;
     Vector3 movePos;
@@ -17,90 +25,152 @@ public class PlayerController2D : MonoBehaviour
     CharacterController characterController;
     GameObject mainCamera;
 
+    public float testX, testY;
+
+
     public enum State
     {
         IDLE,
         WALK,
         JUMP,
         DJUMP,
-        SLIDE,
         SIT,
+        UPVIEW,
     }
 
     public State playerState = State.IDLE;
 
-    // Start is called before the first frame update
     void Start()
     {
+        playerPos = GameObject.Find("Player").GetComponent<Transform>();
         characterController = gameObject.GetComponent<CharacterController>();
         mainCamera = GameObject.Find("Main Camera");
+        puzzle = GameObject.Find("Puzzle").GetComponent<Puzzle>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float xpos = Input.GetAxis("Horizontal") * speed;
         float ypos = Input.GetAxis("Vertical");
+        testX = xpos;
+        testY = ypos;
+
         float movementDirectionY = movePos.y;
 
-        
         if (ypos < 0 && characterController.isGrounded)
         {
             xpos = 0;
-            lookDown = true;
+            ChangeState(State.SIT);
         }
         else
         {
-            lookDown = false;
+            if (characterController.isGrounded)
+            {
+                if (xpos == 0)
+                {
+                    ChangeState(State.IDLE);
+                }
+                else
+                {
+                    ChangeState(State.WALK);
+                }
+            }
         }
         movePos = (forward * xpos);
 
-        /*if (Input.GetButton("Jump") && characterController.isGrounded)
+        if (playerState != State.SIT || playerState != State.JUMP)
         {
-            movePos.y = jumpSpeed;
+            if (ypos > 0 && !GetSpeed())
+            {
+                ChangeState(State.UPVIEW);
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && playerState == State.JUMP)
+        {
+            ChangeState(State.DJUMP);
         }
         else
         {
             movePos.y = movementDirectionY;
+        }
+
+        if (Input.GetButtonDown("Jump") && characterController.isGrounded)
+        {
+            ChangeState(State.JUMP);
+        }
+
+        switch (playerState)
+        {
+            case State.IDLE:
+                IDLE();
+                break;
+            case State.WALK:
+                WALK();
+                break;
+            case State.JUMP:
+                JUMP();
+                break;
+            case State.DJUMP:
+                DJUMP();
+                break;
+            case State.SIT:
+                SIT();
+                break;
+            case State.UPVIEW:
+                UPVIEW();
+                break;
+        }
+
+        //Spring Jump
+        if (jumps)
+        {
+            movePos.y = 12;
+            curTime += Time.deltaTime;
+            if (curTime > coolTime)
+            {
+                jumps = false;
+                curTime = 0;
+            }
+        }
+
+        /*if (puzzle.nextPuzzle) // Next Scene
+        {
+            playerPos.transform.position = new Vector3(0, 2.5f, 38);
+            if (playerPosChange)
+            {
+                characterController.Move(movePos * Time.deltaTime);
+            }
+        }
+        else
+        {
+            characterController.Move(movePos * Time.deltaTime);
         }*/
-
-        if (Input.GetButton("Jump") && characterController.isGrounded)
-        {
-            movePos.y = jumpSpeed;
-        }
-        else
-        {
-            movePos.y = movementDirectionY;
-        }
-
-        
-
-        
-
-        //cameraTarget = transform.position + new Vector3(cameraRange, 1.5f, 0);
-
-        if (lookDown)
-        {
-            cameraTarget = transform.position + new Vector3(cameraRange, -lookYLimit, 0);
-        }
-        else
-        {
-            if(ypos > 0 && !GetSpeed())
-            {
-                cameraTarget = transform.position + new Vector3(cameraRange, lookYLimit*2, 0);
-            }
-            else
-            {
-                cameraTarget = transform.position + new Vector3(cameraRange, 1.5f, 0);
-            }
-        }
-
         characterController.Move(movePos * Time.deltaTime);
-
-        movePos.y -= gravity * Time.deltaTime;
-
         mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, cameraTarget, lookSpeed * Time.deltaTime);
+        movePos.y -= gravity * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "SpawnReset")
+        {
+            playerPosChange = false;
+            puzzle.nextPuzzle = false;
+        }
+
+        //Next Stage
+        if(other.gameObject.tag == "SpawnPoint")
+        {
+            playerPosChange = true;
+        }
+        
+        //Spring Jump
+        if (other.gameObject.tag == "Trap")
+        {
+            jumps = true;
+        }
     }
 
     public bool GetSpeed()
@@ -128,11 +198,11 @@ public class PlayerController2D : MonoBehaviour
             case State.DJUMP:
                 SetDJUMP();
                 break;
-            case State.SLIDE:
-                SetSLIDE();
-                break;
             case State.SIT:
                 SetSIT();
+                break;
+            case State.UPVIEW:
+                SetUPVIEW();
                 break;
         }
 
@@ -141,27 +211,27 @@ public class PlayerController2D : MonoBehaviour
 
     void IDLE()
     {
-
+        cameraTarget = transform.position + new Vector3(cameraRange, 1.5f, 0);
     }
     void WALK()
     {
-
+        cameraTarget = transform.position + new Vector3(cameraRange, 1.5f, 0);
     }
     void JUMP()
     {
-
+        cameraTarget = transform.position + new Vector3(cameraRange, 1.5f, 0);
     }
     void DJUMP()
     {
-
-    }
-    void SLIDE()
-    {
-
+        cameraTarget = transform.position + new Vector3(cameraRange, 1.5f, 0);
     }
     void SIT()
     {
-
+        cameraTarget = transform.position + new Vector3(cameraRange, -lookYLimit, 0);
+    }
+    void UPVIEW()
+    {
+        cameraTarget = transform.position + new Vector3(cameraRange, lookYLimit * 2, 0);
     }
 
     void SetIDLE()
@@ -174,17 +244,17 @@ public class PlayerController2D : MonoBehaviour
     }
     void SetJUMP()
     {
-
+        movePos.y = jumpSpeed;
     }
     void SetDJUMP()
     {
-
+        movePos.y = jumpSpeed;
     }
-    void SetSLIDE()
+    void SetSIT()
     {
 
     }
-    void SetSIT()
+    void SetUPVIEW()
     {
 
     }
